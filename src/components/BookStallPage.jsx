29d25e1495b45f74b4f292357e3f7a7e1ae4e6fe
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-24 20:31:57 +0600

    hg

--- src/components/BookStallPage.jsx
+++ src/components/BookStallPage.jsx
@@ -5,6 +5,31 @@ import { Link, useNavigate } from "react-router";
 const BookStallPage = () => {
  const navigate = useNavigate();
 
+ const date = new Date();
+ const year = date.toISOString().split("-")[0];
+ //  console.log(year);
+
+ const cards = [
+  {
+   image: img1,
+   title: "The Great Adventure",
+   description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus deserunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt!",
+  },
+  {
+   image: img1,
+   title: "The Great Adventure",
+   description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus deserunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt!",
+  },
+  {
+   image: img1,
+   title: "The Great Adventure",
+   description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus deserunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt!",
+  },
+ ];
+
  return (
   <div className="max-w-7xl mx-auto bg-gray-100 text-gray-900">
    {/* Header Section */}
@@ -24,23 +49,22 @@ const BookStallPage = () => {
    <section className="py-12 px-6">
     <h2 className="text-3xl font-semibold text-center mb-6">Featured Books</h2>
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-     <div className="bg-white shadow-md rounded-md p-4 text-center">
-      <img
-       src={img1}
-       alt="Book 1"
-       className="w-full h-48 object-cover rounded-md"
-      />
-      <h3 className="mt-4 font-medium">The Great Adventure</h3>
-      <p>
-       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus,
-       deserunt!
-      </p>
-     </div>
+     {cards.map((card, idx) => (
+      <div key={idx} className="bg-white shadow-md rounded-md p-4 text-center">
+       <img
+        src={card.image}
+        alt="Book 1"
+        className="w-full h-48 object-cover rounded-md"
+       />
+       <h3 className="mt-4 font-medium">{card.title}</h3>
+       <p className="truncate">{card.description}</p>
+      </div>
+     ))}
     </div>
    </section>
 
    <footer className="bg-blue-600 text-white text-center py-4">
-    <p>&copy; 2024 Book Haven. All Rights Reserved.</p>
+    <p>&copy; {year} Book Haven. All Rights Reserved.</p>
    </footer>
   </div>
  );
