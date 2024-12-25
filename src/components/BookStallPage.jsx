ad669ec0d2ed1fd480499eade74990f7d19dc065
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-25 20:36:26 +0600

    gh

--- src/components/BookStallPage.jsx
+++ src/components/BookStallPage.jsx
@@ -1,34 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import img1 from "../assets/book_Img.jpg";
 import { Link, useNavigate } from "react-router";
 
 const BookStallPage = () => {
+ const [showBooks, setShowBooks] = useState([]);
+
+ const [Idelete, setIDelete] = useState([]);
+
+ useEffect(() => {
+  fetch("http://localhost:5000/books")
+   .then((res) => res.json())
+   .then((data) => setShowBooks(data));
+ }, []);
+
  const navigate = useNavigate();
 
  const date = new Date();
  const year = date.toISOString().split("-")[0];
- //  console.log(year);
+ //  console.log(showBooks);
 
- const cards = [
-  {
-   image: img1,
-   title: "The Great Adventure",
-   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus deserunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt!",
-  },
-  {
-   image: img1,
-   title: "The Great Adventure",
-   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus deserunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt!",
-  },
-  {
-   image: img1,
-   title: "The Great Adventure",
-   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus deserunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, deserunt!",
-  },
- ];
+ const handleClick = (_id) => {
+  fetch(`http://localhost:5000/books/${_id}`, {
+   method: "DELETE",
+  })
+   .then((res) => res.json())
+   .then((data) => {
+    console.log(data);
+   });
+ };
 
  return (
   <div className="max-w-7xl mx-auto bg-gray-100 text-gray-900">
@@ -49,15 +48,21 @@ const BookStallPage = () => {
    <section className="py-12 px-6">
     <h2 className="text-3xl font-semibold text-center mb-6">Featured Books</h2>
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-     {cards.map((card, idx) => (
+     {showBooks.map((card, idx) => (
       <div key={idx} className="bg-white shadow-md rounded-md p-4 text-center">
        <img
         src={card.image}
         alt="Book 1"
         className="w-full h-48 object-cover rounded-md"
        />
-       <h3 className="mt-4 font-medium">{card.title}</h3>
+       <h3 className="mt-4 font-medium">{card.name}</h3>
        <p className="truncate">{card.description}</p>
+       <button
+        onClick={() => handleClick(card._id)}
+        className="bg-red-600 text-white font-semibold px-5 py-2 mt-3"
+       >
+        Detele
+       </button>
       </div>
      ))}
     </div>
